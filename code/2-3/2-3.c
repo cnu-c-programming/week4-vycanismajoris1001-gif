09280eeeba80b1cc2e172a9c53a2cad5eba67ba9
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-03-27 09:37:12 +0900

    Update 2-3.c

---
+++ code/2-3/2-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char a[64] = "Hello";
+  char b[64] = "world";
+  char c[128];
+
+  len = strlen(a);
+  printf("length of a: %d\n", len);
+
+  strcpy(c, a);
+  printf("a: %s, c: %s\n", a, c);
+
+  strcat(c, b);
+  printf("c: %s\n", c);
+
+  printf("strcmp(%s, %s) = %d\n", a, a, strcmp(a, a));
+  printf("strcmp(%s, %s) = %d\n", a, b, strcmp(a, b));
+    
