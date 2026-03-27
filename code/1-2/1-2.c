e9e29c558a0304449ebe87c700b6c7e2667db864
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-03-27 09:07:14 +0900

    Update 1-2.c

--- code/1-2/1-2.c
+++ code/1-2/1-2.c
@@ -3,8 +3,8 @@
 #include "util.h"
 
 int main() {
-  for (int i=0;i<100;i++) {
-      if(i%2==0)
+  for (int i=0; i<100; i++) {
+      if(i%2 == 0)
         logger("even");
       else
         logger("odd");
