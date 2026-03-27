d0bd2100dd3979ab39bb513f6c8d1a6f74c9d57d
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-03-27 10:36:29 +0900

    Update 4-1.c

--- code/4-1/4-1.c
+++ code/4-1/4-1.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
 int main() {
-  int a[5] = {1,2,3,4,5};
+  int a[5] = {1, 2, 3, 4, 5};
 
   for (int i=0; i<5; i++)
     printf("%d", a[i]);
   printf("\n");
 
-int b[5] = {1,2};
+int b[5] = {1, 2};
 
-for (int i=0; i<5; i++)
+ for (int i=0; i<5; i++)
     printf("%d", b[i]);
-  printf("\n");
+ printf("\n");
 
 int c[5] = {0};
 
-for (int i=0; i<5; i++)
+ for (int i=0; i<5; i++)
     printf("%d", c[i]);
-  printf("\n");
+ printf("\n");
 
 int d[] = {10, 20, 30};
 
 for (int i=0; i<(sizeof(d)/sizeof(int)); i++)
-    printf("%d", c[i]);
-  printf("\n");
+    printf("%d", d[i]);
+printf("\n");
 
 return 0;
 }
