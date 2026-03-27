f26b34824bd80b37b019965e2d474b488e88e0fe
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-03-27 13:46:36 +0900

    Update 3-2.c

--- code/3-2/3-2.c
+++ code/3-2/3-2.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 void swap_endian(int *x) {
-   
+   unsigned char* p = (unsigned char*)x;
+    unsigned char temp;
+
+    temp = p[0];
+    p[0] = p[3];
+    p[3] = temp;
+
+    temp = p[1];
+    p[1] = p[2];
+    p[2] = temp;
 }
 
 int main() {
